a58b84be7ba35094ce9a3872f79c9b66f19b5d30
Md. Abdul Adud <mdabduladud8@gmail.com>
2024-09-26 16:29:40 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,2 @@
+var a = "Welcome to JavaScript";
+console.log(a); //output
---
+++ variable.js
@@ -0,0 +1,35 @@
+// vaiable declaration
+// var vaiable_name = value;
+
+// variable list
+// var, let
+
+
+// var variable
+
+var myname = "Hello Javascript";  // string
+var b = 10; // number
+var first = 5;
+var secound = 6;
+var total = first + secound;
+
+var firstname = "Md. Abdul";
+var lastname = " Wadud";
+var fullname = firstname + lastname;
+
+
+console.log(firstname,lastname);
+console.log(fullname);
+
+
+
+
+
+// console.log(myname);
+// console.log(); // javascript output
+
+// console.log("First value is "+first);
+// console.log("Second value is "+secound);
+// console.log("Total value is "+total);
+
+// console.log("First Value is "+first, " Second Value is "+ secound, "Total Value is "+ total);
